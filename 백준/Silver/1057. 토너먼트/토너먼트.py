d4ed9716eb59917992664e07cf0e4210120c9344
신효섭 <101155566+sms291@users.noreply.github.com>
2023-05-05 20:52:57 +0900

    [Silver IV] Title: 토너먼트, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -0,0 +1,18 @@
+N,A,B=list(map(int,input().split()))
+
+answer=1
+if A>B:
+    A,B=B,A
+while(1):
+    if (((A//2)+1)==B//2) and(A+1==B):
+        break
+    if A%2==0:
+        A=A//2
+    else:
+        A=(A//2)+1
+    if B%2==0:
+        B=B//2
+    else:
+        B=(B//2)+1    
+    answer+=1
+print(answer)
