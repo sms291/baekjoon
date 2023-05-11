aa21d81988a60d70774846ea1db759292b30144d
신효섭 <101155566+sms291@users.noreply.github.com>
2023-05-11 11:43:31 +0900

    [level 1] Title: 소수 찾기, Time: 181.42 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,12 @@
 def solution(n):
     answer = 0
-    a=[False,False]+[True]*(n-1)
-    prime=[]
-
+    sosu=[0,0]+[1]*(n-1)
+    primes=[]
     for i in range(2,n+1):
-        if a[i]:
-            prime.append(i)
-            for j in range(2*i, n+1, i):
-                a[j] = False
-    answer=len(prime)
+        if sosu[i]==1:
+            primes.append(i)
+            for k in range(2*i,n+1,i):
+                sosu[k]=0
+    answer=len(primes)
+            
     return answer
