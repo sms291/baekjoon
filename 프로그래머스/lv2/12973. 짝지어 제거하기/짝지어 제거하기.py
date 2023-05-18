cc1cc44aa229366de93074698dc9b38bad76411c
신효섭 <101155566+sms291@users.noreply.github.com>
2023-05-18 10:03:42 +0900

    [level 2] Title: 짝지어 제거하기, Time: 94.51 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,15 +1,19 @@
-def solution(s):
-    mun=[]
-    for idx,i in enumerate(s):
-        if not mun:
-            mun.append(i)
+def solution (s):
+# 답안 작성 부분 ===============
+    answer=0
+    total=[]
+    for i in s:
+        if not total:
+            total.append(i)
         else:
-            if i==mun[-1]:
-                mun.pop()
+            if total[-1]==i:
+                total.pop()
             else:
-                mun.append(i)
-    if mun:
-        answer=0
-    else:
+                total.append(i)
+    if len(total)==0:
         answer=1
+    else:
+        answer=0
+   
+# ==============================
     return answer
