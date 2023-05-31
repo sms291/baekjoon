b2f21d0fb65499cf22062ce666b5d76f28fcb8e6
신효섭 <101155566+sms291@users.noreply.github.com>
2023-06-01 07:13:07 +0900

    [level 2] Title: 타겟 넘버, Time: 1.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,3 @@
 def solution(numbers, target):
     answer = 0
-    box=[0]
-    num_case=[]
-    for num in numbers:
-        box2=[]
-        for b in box:
-            box2.append(b+num)
-            box2.append(b-num)
-        box=box2
-    answer=box.count(target)
     return answer
