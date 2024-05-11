88e688839634b187fd6b6a509eacbd2a9ef645c8
신효섭 <101155566+sms291@users.noreply.github.com>
2024-05-11 15:20:36 +0900

    [Silver II] Title: 나무 자르기, Time: 4780 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,7 +2,7 @@ import sys
 A,B=map(int,input().split())
 # K=[int(sys.stdin.readline()) for _ in range(A)]
 K = list(map(int,input().split()))
-start,end = 1,sum(K)
+start,end = 1,max(K)
 while start<=end:
     mid = (start + end) // 2 #중간 위치
     lines = 0 #랜선 수
